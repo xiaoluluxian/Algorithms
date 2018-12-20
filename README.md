865798d9342d9b24efa99484883e7f95d55dd5a9
xiaoluluxian <yuanyingbo@hotmail.com>
2018-12-20 10:24:22 -0600

    5. ToLowerCase

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Collection of Algorithm Questions
 2. Range Sum BST
 3. Max Increase To Keep Skyline
 4. Unique Email
+5. To Lower Case
--- src/Algorithms.java
+++ src/Algorithms.java
@@ -192,5 +192,38 @@ public class Algorithms {
         return set.size();
     }
 
+    /**
+     * 5.
+     *
+     * Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+     *
+     * Example 1:
+     * Input: "Hello"
+     * Output: "hello"
+     *
+     * Example 2:
+     * Input: "here"
+     * Output: "here"
+     *
+     * Example 3:
+     * Input: "LOVELY"
+     * Output: "lovely"
+     *
+     *
+     * @param str
+     * @return
+     */
+
+    public String toLowerCase(String str){
+        int l = str.length();
+        char[] c = str.toCharArray();
+        for(int i = 0;i < l;i++) {
+            if((c[i] <= 'Z')&&(c[i] >= 'A')){
+                c[i] = (char)(str.charAt(i) + 32);
+            }
+        }
+        return new String(c);
+    }
+
 
 }
